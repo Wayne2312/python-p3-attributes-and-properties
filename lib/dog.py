c77120b9acd219f37fe50ecd2026965c1ee1ef91
Wayne2312 <Wayne.Kimani.805@gmail.com>
2025-05-14 11:57:48 +0300

    Add files via upload

---
+++ lib/dog.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+APPROVED_BREEDS = [
+    "Mastiff",
+    "Chihuahua",
+    "Corgi",
+    "Shar Pei",
+    "Beagle",
+    "French Bulldog",
+    "Pug",
+    "Pointer"
+]
+
+
+class Dog:
+    def __init__(self, breed="Pug", name="ValidName"):  # Default valid name
+        self._name = None
+        self._breed = None
+        self.breed = breed  # Validate breed first
+        self.name = name  # Then validate name
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value in APPROVED_BREEDS:
+            self._breed = value
+        else:
+            print("Breed must be in list of approved breeds.")
